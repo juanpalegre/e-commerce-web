7cdc78317574bb8c7b9cc0a4c65832a09cf22de5
juanpalegre <juan.pablo.alegre@it4w.net>
2023-07-30 08:28:35 -0300

    API3 finalizada

--- js/carrito.js
+++ js/carrito.js
@@ -1,15 +1,26 @@
 import productos from "./productos.js"
 
-//Creamos el array vacío
-const carritoFrutas = []
+const almacenarCarrito = () => {
+    if( carritoFrutas.length > 0 ){
+        let carritoConvertido = JSON.stringify(carritoFrutas)
+        localStorage.setItem("carritoFrutas",carritoConvertido)
+    }
+}
 
-//Creamos la función 
 let agregarAlCarrito = (frutaId) => {
     if (frutaId > 0){
         const encontrado = productos.find(producto => producto.id === parseInt(frutaId))
         if(typeof encontrado !== 'undefined') carritoFrutas.push(encontrado)
-        console.table(carritoFrutas)
+        almacenarCarrito()
     }
 }
+
+const recuperarCarrito = () => {
+    return JSON.parse(localStorage.getItem("carritoFrutas")) || []
+}
+
+const carritoFrutas = recuperarCarrito()
+
+console.table(carritoFrutas);
 
 export default agregarAlCarrito;
